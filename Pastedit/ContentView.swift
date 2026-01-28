e34d6e934aefbf5ac1ebd0beceb149fdb5ab764b
Liam O'Donnell <lleeum@gmail.com>
2026-01-28 16:31:08 +0000

    Initial Commit

---
+++ Pastedit/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Pastedit
+//
+//  Created by Liam O'Donnell on 28/01/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
